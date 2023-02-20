13ac0b5266d398afe1cac1d200e3e44a4e543e5f
Wasi-25 <wasiullahazmi36@gmail.com>
2023-02-20 20:21:52 +0530

    initial commit

---
+++ public/js/main.js
@@ -0,0 +1,44 @@
+const cityName = document.getElementById('cityName');
+const submitBtn = document.getElementById("submitBtn");
+const city_name = document.getElementById('city_name');
+const temp_real_val = document.getElementById('temp_real_val');
+const temp_status = document.getElementById('temp_status');
+const datahide= document.querySelector(".middle_layer");
+const getInfo= async(event)=>{
+    event.preventDefault();
+    let cityVal =cityName.value;
+    if(cityVal==""){
+        city_name.innerText = `Please enter the city name before search`;
+        datahide.classList.add('data_hide');
+    }else{
+        try{
+            let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=7cb7c488ee53fee5df76335ef3205522`;
+            const response = await fetch(url);
+            const data = await response.json();
+            const arrData=[data];
+            city_name.innerText = `${arrData[0].name},${arrData[0].sys.country}`;
+            temp_real_val.innerText= arrData[0].main.temp;
+
+            const tempMood = arrData[0].weather[0].main;
+
+            // condition to check temp status
+            if(tempMood =="Clear"){
+                temp_status.innerHTML= "<i class='fas fa-sun' style='color: #eccc68;'></i>";
+            }else if(tempMood=="Clouds"){
+                temp_status.innerHTML= "<i class='fas fa-cloud' style='color: #f1f2f6;'></i>";
+            }else if(tempMood=="Rain"){
+                temp_status.innerHTML= "<i class='fas fa-cloud-rain' style='color: #a4b0be;'><i>";
+            }else if(tempMood=="Smoke"){
+                temp_status.innerHTML= "<i class='fas fa-smog' style='color: #f1f2f6;'></i>";
+            }else{
+                temp_status.innerHTML= "<i class='fas fa-sun' style='color: #eccc68;'></i>";
+            }  
+            datahide.classList.remove('data_hide');
+            }catch{
+            city_name.innerText = `Please enter the city name properly`;
+            datahide.classList.add('data_hide');
+        }
+    }
+}
+
+submitBtn.addEventListener('click',getInfo);
---
+++ src/app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app =express();
+const hbs=require("hbs");
+const path = require("path");
+const port = process.env.PORT;
+
+// public static path
+const staticPath= path.join(__dirname,"../public");
+const templatePath= path.join(__dirname,"../templates/views");
+const partialsPath= path.join(__dirname,"../templates/partials");
+app.set("view engine", "hbs");
+app.set("views",templatePath);
+hbs.registerPartials(partialsPath);
+app.use(express.static(staticPath));
+
+//routing
+app.get("",(req,res)=>{
+    res.render("index");
+});
+app.get("/about",(req,res)=>{
+    res.render("about");
+});
+app.get("/weather",(req,res)=>{
+    res.render("weather");
+});
+app.get("*",(req,res)=>{
+    res.render("404error",{
+        errorMsg: "Oops! Page Doesn't Exists, Please Go Back"
+    });
+});
+app.listen(port,()=>{
+    console.log(`Listening to port ${port}`);
+});
